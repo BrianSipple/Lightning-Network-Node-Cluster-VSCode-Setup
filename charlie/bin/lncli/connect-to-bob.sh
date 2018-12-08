820326f05e106ad5d3cde2b58d75c33c57ee2b5d
Brian Sipple <brsipple42@gmail.com>
2018-12-08 02:56:26 -0500

    connect nodes

--- charlie/bin/lncli/connect-to-bob.sh
+++ charlie/bin/lncli/connect-to-bob.sh
@@ -1,12 +1,12 @@
 ########################################################################
-# Connect Alice's LND node to Bob's
+# Connect Charlie's LND node to Bob's
 #
 # This involves specifying Bob's `identity_pubkey`, which can be found
 # running `lncli getinfo` for his node.
 ########################################################################
 
 lncli \
-  --rpcserver=localhost:10001 \
+  --rpcserver=localhost:10003 \
   --macaroonpath=data/chain/bitcoin/simnet/admin.macaroon \
   connect \
   0369368e0062185bb10add0ccc1c74b8882a51a8af616c34fab306e09428151125@localhost:10012
---
+++ charlie/bin/lncli/list-peers.sh
@@ -0,0 +1,4 @@
+lncli \
+  --rpcserver=localhost:10003 \
+  --macaroonpath=data/chain/bitcoin/simnet/admin.macaroon \
+  listpeers
